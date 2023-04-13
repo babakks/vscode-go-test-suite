08643e8a8638abdbab73cfca23a0cbf96fe2e9b3
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2023-04-13 13:47:20 +0000

    📦 Bootstrap `quicktest` test provider
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,29 @@
-import * as vscode from 'vscode';
-import { GocheckTestLibraryAdapter, TestProvider } from './testProvider';
 import { mkdirSync } from 'fs';
+import * as vscode from 'vscode';
+import { GocheckTestLibraryAdapter, QtsuiteTestLibraryAdapter, TestProvider } from './testProvider';
 
 export function activate(context: vscode.ExtensionContext) {
     mkdirSync(context.logUri.fsPath, { recursive: true });
-    const gocheckController = vscode.tests.createTestController('gocheck', 'Go (gocheck)');
-    const gocheckOutput = vscode.window.createOutputChannel('Go (gocheck)');
-    const gocheckAdapter = new GocheckTestLibraryAdapter();
-    const gocheckTestProvider = new TestProvider(gocheckController, gocheckOutput, gocheckAdapter, context.logUri);
-    context.subscriptions.push(gocheckController, gocheckOutput, gocheckTestProvider);
+    context.subscriptions.push(
+        ...setupGocheckTestProvider(context),
+        ...setupQtsuiteTestProvider(context),
+    );
 }
 
 export function deactivate() { }
 
+function setupGocheckTestProvider(context: vscode.ExtensionContext): vscode.Disposable[] {
+    const gocheckController = vscode.tests.createTestController('gocheck', 'Go (gocheck)');
+    const gocheckOutput = vscode.window.createOutputChannel('Go (gocheck)');
+    const gocheckAdapter = new GocheckTestLibraryAdapter();
+    const gocheckTestProvider = new TestProvider(gocheckController, gocheckOutput, gocheckAdapter, context.logUri);
+    return [gocheckController, gocheckOutput, gocheckTestProvider];
+}
+
+function setupQtsuiteTestProvider(context: vscode.ExtensionContext): vscode.Disposable[] {
+    const qtsuiteController = vscode.tests.createTestController('qtsuite', 'Go (qtsuite)');
+    const qtsuiteOutput = vscode.window.createOutputChannel('Go (qtsuite)');
+    const qtsuiteAdapter = new QtsuiteTestLibraryAdapter();
+    const qtsuiteTestProvider = new TestProvider(qtsuiteController, qtsuiteOutput, qtsuiteAdapter, context.logUri);
+    return [qtsuiteController, qtsuiteOutput, qtsuiteTestProvider];
+}
